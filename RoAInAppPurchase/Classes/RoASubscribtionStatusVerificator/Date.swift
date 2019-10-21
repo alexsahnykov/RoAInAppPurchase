d7d69b5e9fb27be26d5783778d04160cf4ba4b87
Alex Sahnykov <alexsahnykov@gmail.com>
2019-10-21 15:44:56 +0300

    test replace

--- RoAInAppPurchase/Classes/RoASubscribtionStatusVerificator/Date.swift
+++ RoAInAppPurchase/Classes/RoASubscribtionStatusVerificator/Date.swift
@@ -5,20 +5,4 @@
 //  Created by Александр Сахнюков on 21/08/2019.
 //
 
-extension Date {
-   
-    public static func getTodayRounded() -> Date {
-        let now = Date()
-        
-        let gregorian = Calendar(identifier: .gregorian)
-        
-        let dateComponents = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
-        let dateWithSelectedComponents = gregorian.date(from: dateComponents)!
-        
-        return dateWithSelectedComponents
-    }
-    
-   public func toISOString(formatter: DateFormatter) -> String {
-        return formatter.string(from: self)
-    }
-}
+
